c6160279199be4e147ce9d093f83a1d9335ae16e
Pranav K <prkrishn@hotmail.com>
2022-02-22 16:02:17 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO.Compression;
+
+var pwd = Path.Combine(Directory.GetCurrentDirectory(), "test");
+var stream = File.Create(@"out.zip");
+var directory = new DirectoryInfo(pwd);
+
+using (var zip = new ZipArchive(stream, ZipArchiveMode.Create, true))
+{
+    foreach (FileInfo file in directory.EnumerateFiles("*", SearchOption.AllDirectories))
+    {
+        string relativePath =
+            file.FullName.Substring(pwd.Length + 1); // +1 prevents it from including the leading backslash
+        string zipEntryName = relativePath.Replace('\\', '/'); // Normalize slashes
+
+        zip.CreateEntryFromFile(file.FullName, zipEntryName);
+    }
+}
